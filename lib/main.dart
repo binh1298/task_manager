9b3b1f37dc352d2d683697bb6429cce71042c575
Binh Pham <binh1298@gmail.com>
2020-03-05 04:57:56 +0700

    Check token for session, remove unnecessary comment

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,18 @@ import 'package:task_manager/models/user_details.dart';
 import 'package:task_manager/screens/admin/home.dart';
 import 'package:task_manager/screens/login.dart';
 import 'package:task_manager/utils/secure_storage.dart';
+
 void main() async {
   await DotEnv().load('.env');
   String token = await getJwtToken();
-  final userJson = parseJwt(token);
-  final UserDetails user = UserDetails.fromJson(userJson);
-  runApp(MyApp(user: user));
+  if (token != null) {
+    final userJson = parseJwt(token);
+    final UserDetails user = UserDetails.fromJson(userJson);
+    runApp(MyApp(user: user));
+  }
+  runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   final UserDetails user;
   MyApp({Key key, this.user}) : super(key: key);
@@ -20,19 +25,12 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-
       ),
-      initialRoute: (user != null && user.roleName == 'admin') ? '/admin' : '/login',
+      initialRoute:
+          (user != null && user.roleName == 'admin') ? '/admin' : '/login',
       routes: {
         '/login': (context) => LoginScreen(),
         '/admin': (context) => HomeScreen(),
-
-        // Admin
-        // '/userList': (context) => ViewUsersScreen(),
-        // '/viewUserInfo': (context) => ViewUserInfo(),
-        // '/createUser': (context) => CreateUserScreen(),
-        // '/createGroup': (context) => CreateGroupScreen(),
-        
       },
     );
   }
