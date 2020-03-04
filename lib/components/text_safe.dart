ffb1566419441de596438d88eecf8eaa161cdcb3
Binh Pham <binh1298@gmail.com>
2020-03-05 04:14:03 +0700

    Change behavior when text overflows

--- lib/components/text_safe.dart
+++ lib/components/text_safe.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// In order to use this you have to always set maxwidth for the parent
 
 class TextSafeComponent extends StatefulWidget {
   final String text;
@@ -12,9 +13,14 @@ class TextSafeComponent extends StatefulWidget {
 class _TextSafeComponentState extends State<TextSafeComponent> {
   @override
   Widget build(BuildContext context) {
-    return Text(
-      widget.text == null ? 'Unknown' : widget.text,
-      style: widget.style,
+    return Container(
+      width: 220,
+      child: Text(
+        widget.text == null ? 'Unknown' : widget.text,
+        overflow: TextOverflow.ellipsis,
+        maxLines: 5,
+        style: widget.style,
+      ),
     );
   }
 }
