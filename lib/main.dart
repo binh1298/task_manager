d36be8eb655b6bb5de9bdcc3ad6dcd98ae9f9186
Binh Pham <binh1298@gmail.com>
2020-03-05 04:37:33 +0700

    Implement user session

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:task_manager/models/user_details.dart';
 import 'package:task_manager/screens/admin/home.dart';
 import 'package:task_manager/screens/login.dart';
+import 'package:task_manager/utils/secure_storage.dart';
 void main() async {
   await DotEnv().load('.env');
-  runApp(MyApp());
+  String token = await getJwtToken();
+  final userJson = parseJwt(token);
+  final UserDetails user = UserDetails.fromJson(userJson);
+  runApp(MyApp(user: user));
 }
 class MyApp extends StatelessWidget {
+  final UserDetails user;
+  MyApp({Key key, this.user}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,7 +22,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
 
       ),
-      initialRoute: '/login',
+      initialRoute: (user != null && user.roleName == 'admin') ? '/admin' : '/login',
       routes: {
         '/login': (context) => LoginScreen(),
         '/admin': (context) => HomeScreen(),
