aebd066fdffc0f605923ec4af2dccb63901cd6cb
Binh Pham <binh1298@gmail.com>
2020-03-04 23:49:39 +0700

    Change color for form button

--- lib/components/button_confirm.dart
+++ lib/components/button_confirm.dart
@@ -17,7 +17,7 @@ class _ButtonConfirmComponentState extends State<ButtonConfirmComponent> {
     return Material(
       elevation: 5.0,
       borderRadius: BorderRadius.circular(30.0),
-      color: Color(0xff01A0C7),
+      color: colorPrimary,
       child: MaterialButton(
         minWidth: MediaQuery.of(context).size.width,
         padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
