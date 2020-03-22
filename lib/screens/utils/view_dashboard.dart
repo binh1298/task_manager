2efde0b1be626e70bbf14b7aca065e422a150f60
phamducbinh <binh1298@gmail.com>
2020-03-22 14:18:36 +0700

    Add user list to dashboard

--- lib/screens/utils/view_dashboard.dart
+++ lib/screens/utils/view_dashboard.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager/components/cards/card_user_list_item.dart';
 import 'package:task_manager/components/labels/text_safe.dart';
 import 'package:task_manager/components/scan_button.dart';
 import 'package:task_manager/models/dashboard.dart';
+import 'package:task_manager/models/user_details.dart';
 import 'package:task_manager/style/style.dart';
 import 'package:task_manager/utils/snack_bar.dart';
 import 'package:task_manager/utils/string_utils.dart';
@@ -13,6 +15,7 @@ class ViewDashboard extends StatefulWidget {
 
 class _ViewDashboardState extends State<ViewDashboard> {
   Future<Dashboard> dashboard;
+  Future<List<UserDetails>> usersDetailsList;
   GlobalKey<RefreshIndicatorState> refreshKey =
       GlobalKey<RefreshIndicatorState>();
 
@@ -28,6 +31,7 @@ class _ViewDashboardState extends State<ViewDashboard> {
 
     setState(() {
       dashboard = fetchDashboard();
+      usersDetailsList = fetchUsersDetailsList();
     });
 
     return null;
@@ -50,114 +54,143 @@ class _ViewDashboardState extends State<ViewDashboard> {
       body: RefreshIndicator(
         onRefresh: refreshList,
         key: refreshKey,
-        child: FutureBuilder<Dashboard>(
-          future: dashboard,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return ListView(
-                children: <Widget>[
-                  ScanButton(
-                    context: context,
-                    onSuccessScan: (barcode) {
-                      Navigator.pushNamed(context, '/viewUserDetail',
-                          arguments: barcode);
-                    },
-                    title: 'Scan QR to find Employee',
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        TextSafeComponent(
-                          text: 'Dashboard:',
-                          style: textStyleHeading,
-                        ),
-                        SizedBox(height: 20),
-                        Row(
+        child: ListView(
+          children: <Widget>[
+            FutureBuilder<Dashboard>(
+              future: dashboard,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Column(
+                    children: <Widget>[
+                      ScanButton(
+                        context: context,
+                        onSuccessScan: (barcode) {
+                          Navigator.pushNamed(context, '/viewUserDetail',
+                              arguments: barcode);
+                        },
+                        title: 'Scan QR to find Employee',
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: <Widget>[
                             TextSafeComponent(
-                              text: 'Number of Groups: ',
-                              style: textStyleTitle,
-                              textBoxWidth: null,
+                              text: 'Dashboard:',
+                              style: textStyleHeading,
                             ),
-                            TextSafeComponent(
-                              text: snapshot.data.groups.toString(),
-                              style: textStyleTitle,
-                              textBoxWidth: null,
-                            ),
-                          ],
-                        ),
-                        SizedBox(height: 20),
-                        TextSafeComponent(
-                          text: 'Number of Users:',
-                          style: textStyleTitle,
-                        ),
-                        SizedBox(height: 10),
-                        Column(
-                          children: snapshot.data.usersDashboardInfo
-                              .map(
-                                (usersDashboardInfo) => TextSafeComponent(
-                                  label:
-                                      capitalize(usersDashboardInfo.roleName),
-                                  text: usersDashboardInfo.count.toString(),
-                                  style: textStyleSubtitle,
+                            SizedBox(height: 20),
+                            Row(
+                              children: <Widget>[
+                                TextSafeComponent(
+                                  text: 'Number of Groups: ',
+                                  style: textStyleTitle,
+                                  textBoxWidth: null,
                                 ),
-                              )
-                              .toList(),
-                        ),
-                        SizedBox(height: 20),
-                        TextSafeComponent(
-                          text: 'Number of Tasks:',
-                          style: textStyleTitle,
-                        ),
-                        SizedBox(height: 10),
-                        Column(
-                          children: snapshot.data.tasksDashboardInfo
-                              .map(
-                                (tasksDashboardInfo) => Row(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: <Widget>[
-                                    TextSafeComponent(
-                                      text: tasksDashboardInfo.taskStatus + ': ',
-                                      style: textStyleSubtitle.copyWith(fontWeight: FontWeight.bold),
-                                      color: getTaskStatusColor(
-                                          tasksDashboardInfo.taskStatus),
-                                      textBoxWidth: null,
-                                    ),
-                                    TextSafeComponent(
-                                      text: tasksDashboardInfo.count.toString(),
+                                TextSafeComponent(
+                                  text: snapshot.data.groups.toString(),
+                                  style: textStyleTitle,
+                                  textBoxWidth: null,
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 20),
+                            TextSafeComponent(
+                              text: 'Number of Users:',
+                              style: textStyleTitle,
+                            ),
+                            SizedBox(height: 10),
+                            Column(
+                              children: snapshot.data.usersDashboardInfo
+                                  .map(
+                                    (usersDashboardInfo) => TextSafeComponent(
+                                      label: capitalize(
+                                          usersDashboardInfo.roleName),
+                                      text: usersDashboardInfo.count.toString(),
                                       style: textStyleSubtitle,
                                     ),
-                                  ],
-                                ),
-                              )
-                              .toList(),
+                                  )
+                                  .toList(),
+                            ),
+                            SizedBox(height: 20),
+                            TextSafeComponent(
+                              text: 'Number of Tasks:',
+                              style: textStyleTitle,
+                            ),
+                            SizedBox(height: 10),
+                            Column(
+                              children: snapshot.data.tasksDashboardInfo
+                                  .map(
+                                    (tasksDashboardInfo) => Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: <Widget>[
+                                        TextSafeComponent(
+                                          text: tasksDashboardInfo.taskStatus +
+                                              ': ',
+                                          style: textStyleSubtitle.copyWith(
+                                              fontWeight: FontWeight.bold),
+                                          color: getTaskStatusColor(
+                                              tasksDashboardInfo.taskStatus),
+                                          textBoxWidth: null,
+                                        ),
+                                        TextSafeComponent(
+                                          text: tasksDashboardInfo.count
+                                              .toString(),
+                                          style: textStyleSubtitle,
+                                        ),
+                                      ],
+                                    ),
+                                  )
+                                  .toList(),
+                            ),
+                            SizedBox(height: 20),
+                          ],
                         ),
-                        SizedBox(height: 20),
-                        
-                      ],
-                    ),
-                  ),
-                ],
-              );
-            } else if (snapshot.hasError) {
-              return ListView(
-                children: <Widget>[
-                  Center(
+                      ),
+                    ],
+                  );
+                } else if (snapshot.hasError) {
+                  return Center(
                     child: Text('${snapshot.error}'),
-                  ),
-                ],
-              );
-            }
-            return ListView(
-              children: <Widget>[
-                Center(
+                  );
+                }
+                return Center(
                   child: CircularProgressIndicator(),
-                ),
-              ],
-            );
-          },
+                );
+              },
+            ),
+            FutureBuilder<List<UserDetails>>(
+              future: usersDetailsList,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Column(
+                    children: snapshot.data
+                        .map(
+                          (userDetail) => CardUserListItem(
+                            userId: userDetail.userId,
+                            fullname: userDetail.fullname,
+                            role: userDetail.roleName,
+                            // phoneNumber: userDetail.phoneNumber,
+                            email: userDetail.email,
+                            avatar: userDetail.avatar,
+                            isDeleted: userDetail.isDeleted,
+                            refreshList: refreshList,
+                          ),
+                        )
+                        .toList(),
+                  );
+                } else if (snapshot.hasError) {
+                  return Center(
+                    child: Text('${snapshot.error}'),
+                  );
+                }
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              },
+            ),
+          ],
         ),
       ),
     );
